fc24a80f34d82b9e27f44ff23f211ccc320fb9f8
Alex Perez <alyexp490@gmail.com>
2023-11-16 08:25:39 -0500

    Finished

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -6,13 +6,16 @@ export const TodoForm = ({addTodo}) => {
   const handleSubmit = e => {
     e.preventDefault();
 
-    addTodo(value)
+    addTodo(value);
+
+    setValue("")
+
   }
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
       <input type="text" className='todo-input'
-       placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)}/>
-       <button type='submit' className='todo-btn'>Add Task</button>
+      value={value} placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)}/>
+       <button type='submit' className='todo-btn'>Add Task</button> 
     </form>
 
   )
